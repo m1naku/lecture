7032e20453ef164fd77b2dcb3ae10798cb2838a0
m1naku <kirufan2007@gmail.com>
2025-07-01 19:51:39 +0300

    Реализовал функцию draw_house
    Сочинил её реализацию.
    Нужно сделать еще три функции: для основания,
    стен, крыши.

--- lecture 2.py
+++ lecture 2.py
@@ -1,5 +1,12 @@
 
 
+def main():
+    x, y = 300, 400
+    width, height = 200, 300
+
+    draw_house(x, y, width, height)
+
+
 def draw_house(x, y, width, height):
     """
     Нарисовать домик ширины width и высоты height от опорной точки (x, y),
@@ -11,10 +18,40 @@ def draw_house(x, y, width, height):
     :return: None
     """
     print("like a draw house", x, y, width, height)
+    foundation_height = 0.05 * height
+    walls_height = 0.5 * height
+    walls_width = 0.9 * width
+    roof_height = height - foundation_height - walls_height
+
+    draw_house_foundation(x, y, width, foundation_height)
+    draw_house_walls(x, y - foundation_height, walls_width, walls_height)
+    draw_house_roof(x, y - foundation_height - walls_height, width, roof_height)
+
+
+def draw_house_foundation(x, y, width, height):
+    """
+     Нарисовать основание домик ширины width и высоты height от опорной точки (x, y),
+     которая находится в середине нижней точки фундамента.
+     :param x: координата x середины фундамента
+     :param y: координата y низа фундамента
+     :param width: полная ширина фундамента
+     :param height: полная высота фундамента
+     :return: None
+     """
+    print("like a draw house", x, y, width, height)
     pass
 
 
-x, y = 100, 100
-width, height = 300, 300
+def draw_house_walls(x, y, width, height):
+    print("like a draw house", x, y, width, height)
+    pass
 
-draw_house(x, y, width, height)
+
+def draw_house_roof(x, y, width, height):
+    print("like a draw house", x, y, width, height)
+    pass
+
+
+
+
+main()
